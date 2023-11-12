e1aa265c4897a47c423df7044aa73174d92f45b3
prasanv <bala.prasanv@gmail.com>
2023-11-12 11:46:39 -0800

    add layouts and css files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+// note: the global.css is simply imported at the root layout not called or wrapped around anywhere
+import './global.css'
 
 export default function RootLayout({
     children,
