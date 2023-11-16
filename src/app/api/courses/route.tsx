db9808b7d5e1c9414686cb167cb6d38cb02720b3
prasanv <bala.prasanv@gmail.com>
2023-11-15 17:01:38 -0800

    api routes post request

--- src/app/api/courses/route.tsx
+++ src/app/api/courses/route.tsx
@@ -2,7 +2,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import courses from './data.json';
 
-// API: http://localhost:8000/api/courses
+// GET: http://localhost:8000/api/courses
 export async function GET(request: NextRequest){
     return NextResponse.json(courses);
 }
+
+// POST: http://localhost:8000/api/courses
+export async function POST(request: NextRequest){
+    console.log(request);
+    const { title, description, link, level } = await request.json();
+    
+    courses.push({ id: Math.round(Math.random()*10000)
+        , title, description, link, level });
+    return NextResponse.json(courses);
+}
+
+
+// Sample Body Data for Post request
+// {
+//     "title": "ReactNative for Dev",
+//     "description": "Sharpen your skills by building unique & fun mini mobile projects.",
+//     "link": "https://github.com/prasanv/reactNative",
+//     "level": "Beginner"
+// }
--- src/app/api/courses/search/route.tsx
+++ src/app/api/courses/search/route.tsx
@@ -4,8 +4,15 @@ import courses from '../data.json';
 
 // API: http://localhost:8000/api/courses/search
 export async function GET(request: NextRequest){
-    // Method 1 using URL web API
+    // Method 1 using URL web API https://developer.mozilla.org/en-US/docs/Web/API
+    // Example 1
     // const { searchParams } = new URL(request.nextUrl);
+
+    // Example 2
+    // const myHeader = new Headers(request.headers);
+    // console.log(myHeader);
+    // myHeader.delete('user-agent')
+    // console.log(myHeader);
     
     // Method 2 Directly from request url
     const searchParams = request.nextUrl.searchParams;
@@ -20,10 +27,11 @@ export async function GET(request: NextRequest){
     return NextResponse.json(filteredCourse);
 }
 
-
+// Web API's https://developer.mozilla.org/en-US/docs/Web/API
 // The URL() constructor returns a newly created URL object representing the URL defined by the parameters.
 // const baseUrl = "https://developer.mozilla.org/";
 // let B = new URL(baseUrl);
 // => 'https://developer.mozilla.org/'
 // new URL("en-US/docs", B);
 // => 'https://developer.mozilla.org/en-US/docs'
+
