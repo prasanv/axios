8765ad7bb2984ba78d86f9ed9dab4487f9482109
prasanv <bala.prasanv@gmail.com>
2023-11-15 17:47:05 -0800

    added client comp page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+'use client';
+
+import axios from "axios";
+import React, { useEffect, useState} from "react";
+import Loading from 'src/app/loading'
+import CoursesComponent from "../components/courses";
 
 export default function Page() {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchCourses = async () => {
+    try {
+      const res = await axios.request({
+          url: "http://localhost:8000/api/courses",
+          method: "get"
+      })
+       setCourses(res.data);
+       setLoading(false);
+    } catch (err) {
+        console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchCourses();
+  },[])
+
+  console.log(loading, courses);
+
+  if(loading){
+    return (<Loading></Loading>);
+  }
     return <div>
-      <h1>Hello, Next.js 13  Home Page!</h1>
+      <CoursesComponent courses={courses}/>
     </div>
   }
--- src/components/courses.tsx
+++ src/components/courses.tsx
@@ -1,25 +1,12 @@
-import axios from 'axios'
 import Link from 'next/link'
 import React from 'react'
 import 'src/app/global.css'
 
-const fetchCourses = async () => {
-    try {
-        const res = await axios.request({
-            url: "http://localhost:8000/api/courses",
-            method: "get"
-        })
-        return res.data;
-    } catch (err) {
-        console.log(err);
-    }
-}
 
-const Courses = async () => {
-    const courses = await fetchCourses();
+const Courses = ({ courses }: any) => {
   return (
     <div className='courses'>
-      {courses.map((course: { id: React.Key | null | undefined; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; level: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; description: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; link: string | UrlObject }) => 
+      {courses?.map((course: { id: React.Key | null | undefined; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; level: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; description: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; link: string | UrlObject }) => 
       (<div key={course.id} className='card'>
         <h2>{course.title}</h2>
         <small>Level: {course.level}</small>
